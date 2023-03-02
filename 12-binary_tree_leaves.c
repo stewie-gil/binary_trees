6ed6dc2098d84d7a34026f7b0140b4ebf7426426
stewie-gil <gilliganstewie7@gmail.com>
2023-03-02 10:51:32 -0800

    leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,20 +1,24 @@
 #include "binary_trees.h"
 #include <stdlib.h>
+/**
+* binary_tree_leaves - count leaves
+* @tree: node
+* Return: size_t
+*/
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	{
-		return (0);
-	}
+if (tree == NULL)
+{
+return (0);
+}
 
-	binary_tree_leaves(tree->left);
-	binary_tree_leaves(tree->right);
+binary_tree_leaves(tree->left);
+binary_tree_leaves(tree->right);
 
-	if((tree->left == NULL) && (tree->right == NULL))
-	{
-		return(binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right) + 1 );
-	}
-	else
-		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
-
+if ((tree->left == NULL) && (tree->right == NULL))
+{
+return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right) + 1);
+}
+else
+return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
