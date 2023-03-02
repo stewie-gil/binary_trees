ded47ce30bb9fa5b41a3ffb4f8b79a50102b2da9
stewie-gil <gilliganstewie7@gmail.com>
2023-03-02 03:33:29 -0800

    0-binary_tree_node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * binary_tree_node - creates a binary tree node
+ * Return: newnode
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+binary_tree_t *newnode;
+newnode= malloc(sizeof (binary_tree_t));
+if (newnode == NULL)
+{
+return NULL;
+}
+
+newnode->parent = parent;
+newnode->n = value;
+newnode->left = NULL;
+newnode->right = NULL;
+
+
+
+return newnode;
+
+}
