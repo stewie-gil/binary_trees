82d2af521554b4fee1056f08f4e2bc2dc8f692c7
stewie-gil <gilliganstewie7@gmail.com>
2023-03-02 10:40:49 -0800

    11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,12 +1,17 @@
 #include <stdlib.h>
 #include "binary_trees.h"
+/**
+ * binary_tree_size - counts size
+ * @tree: node
+ * Return: size_t
+ */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	{
-		return 0;
-	}
-	binary_tree_size(tree->left);
-	binary_tree_size(tree->right);
-	return(binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+if (tree == NULL)
+{
+return 0;
+}
+binary_tree_size(tree->left);
+binary_tree_size(tree->right);
+return(binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
