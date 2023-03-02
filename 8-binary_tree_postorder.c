92bfcfe9f8eea5f63feb15fb0aec9b6a0435b746
stewie-gil <gilliganstewie7@gmail.com>
2023-03-02 09:50:34 -0800

    postorder

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,14 +1,18 @@
 #include "binary_trees.h"
 #include <stdlib.h>
+/**
+ * binary_tree_postorder - postorder traversal
+ * @tree: node to pass
+ * @func : function
+ * Return: void
+ */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 if (tree == NULL)
 {
-	return;
 }
-if(func == NULL)
+if (func == NULL)
 {
-	return;
 }
 
 binary_tree_postorder(tree->left, func);
