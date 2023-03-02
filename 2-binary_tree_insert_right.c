fb751216e80d6d090ea8ecf46da9a704fcfd11cd
stewie-gil <gilliganstewie7@gmail.com>
2023-03-02 03:56:53 -0800

    task 2-5

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	/*create new node*/
+
+	binary_tree_t *newnode;
+	binary_tree_t *temp;
+
+	newnode = binary_tree_node(parent, value);
+
+	if (newnode == NULL)
+	{
+		return NULL;
+	}
+
+	if (parent->right == NULL)
+	{
+		parent->right = newnode;
+
+	}
+	else
+	{
+		temp = parent->right;
+		parent->right = newnode;
+		newnode->right=temp;
+	}
+return newnode;
+
+}
