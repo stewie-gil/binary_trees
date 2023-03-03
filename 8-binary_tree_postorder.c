220f299ccfada8664836163671283ac5a8a06649
stewie-gil <gilliganstewie7@gmail.com>
2023-03-03 05:25:42 -0800

    fixed postordr

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -8,15 +8,11 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-if (tree == NULL)
+if (tree && func)
 {
-}
-if (func == NULL)
-{
-}
-
+func(tree->n);
 binary_tree_postorder(tree->left, func);
 binary_tree_postorder(tree->right, func);
 func(tree->n);
-
+}
 }
