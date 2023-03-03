6ddda223f365c6d0f24fcddb624a0a83a9c3b141
stewie-gil <gilliganstewie7@gmail.com>
2023-03-03 04:46:54 -0800

    m

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -18,7 +18,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		left_height = tree->left ? binary_tree_height(tree->left) + 1 : 1;
 		right_height = tree->right ? binary_tree_height(tree->right) + 1 : 1;
 
-		return (MAX(left_height, right_height));
+		if (left_height > right_height)
+		  return left_height;
+		else
+		  return right_height;
+
+		
 	}
 
 	return (0);
